3360d3b8c5e28196c8f52a68aa292bd630127785
SalahMallouh95 <SalahMalouh95@gmail.com>
2023-03-08 11:16:13 +0300

    1

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { MainComponent } from './main/main.component';
+import { ServiceComponent } from './service/service.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MainComponent,
+    ServiceComponent,
+    AboutComponent,
+    ContactComponent
+  ],
   imports: [
     CommonModule,
     HomeRoutingModule
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeHeaderComponent } from './home-header/home-header.component';
+import { HomeFooterComponent } from './home-footer/home-footer.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input'
+
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeHeaderComponent,
+    HomeFooterComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    HomeFooterComponent,
+    HomeHeaderComponent,
+    ReactiveFormsModule,
+    MatInputModule
   ]
 })
 export class SharedModule { }
