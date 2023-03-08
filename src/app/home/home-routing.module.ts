63b9ceb0c69a98294554611c0140db0585115f3d
SalahMallouh95 <SalahMalouh95@gmail.com>
2023-03-08 12:51:13 +0300

    1

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,5 +1,6 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
 import { MainComponent } from './main/main.component';
 
 const routes: Routes = [
@@ -7,6 +8,10 @@ const routes: Routes = [
     path:'',
     component:MainComponent
  
+  },
+  {
+    path:"ContactUs",
+    component:ContactComponent
   }
 ];
 
