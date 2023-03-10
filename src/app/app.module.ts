845a941aa1611494eb6543d8207d59256582eed5
SalahMallouh95 <SalahMalouh95@gmail.com>
2023-03-10 19:00:19 +0300

    2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
-import { NgModule } from '@angular/core';
+import { NgModule,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -17,6 +16,10 @@ import { NgxSpinnerModule } from "ngx-spinner";
     BrowserAnimationsModule,
     NgxSpinnerModule
   ],
+  schemas:[
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
+  ,
   providers: [],
   bootstrap: [AppComponent]
 })
