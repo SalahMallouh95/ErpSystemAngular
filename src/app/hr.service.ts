479cf5e1e0d2e9baead667b5a1b1831295fd9c1e
SalahMallouh95 <SalahMalouh95@gmail.com>
2023-03-10 15:05:34 +0300

    3

--- src/app/hr.service.ts
+++ src/app/hr.service.ts
@@ -8,8 +8,8 @@ export class HrService {
   constructor() { }
    mesage:string="test"
   allLeaves = [
-    { "leaveid": 1, "startDate": Date.now,"endDate":Date.now,"Message":"tet","documentFileName":"ss","leaveType":"lev","fName":"sa","LName":"ma"},
-    { "leaveid": 2, "startDate": Date.now,"endDate":Date.now,"Message":"tet2","documentFileName":"ss2","leaveType":"lev2","fName":"sa2","LName":"ma2"},
-    { "leaveid": 3, "startDate": Date.now,"endDate":Date.now,"Message":"tet3","documentFileName":"ss3","leaveType":"lev3","fName":"sa3","LName":"ma3"}
+    { "leaveid": 1, "startDate": "2222","endDate":"2222","Message":"tet","State":2,"documentFileName":"ss","leaveType":"lev","fName":"Salah","LName":"Malouh"},
+    { "leaveid": 2, "startDate": "2222","endDate":"2222","Message":"tet2","State":0,"documentFileName":"ss2","leaveType":"lev2","fName":"mohamde","LName":"ma2"},
+    { "leaveid": 3, "startDate": "2222","endDate":"2222","Message":"tet3","State":1,"documentFileName":"ss3","leaveType":"lev3","fName":"Moyeed","LName":"ma3"}
   ];
 }
