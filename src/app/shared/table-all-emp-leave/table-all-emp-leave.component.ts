0f68b00e1a6c5364deccc59bd90da0982163cfef
SalahMallouh95 <SalahMalouh95@gmail.com>
2023-03-10 15:15:53 +0300

    2

--- src/app/shared/table-all-emp-leave/table-all-emp-leave.component.ts
+++ src/app/shared/table-all-emp-leave/table-all-emp-leave.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-table-all-emp-leave',
+  selector: 'app-table-all-emp-leave,[app-table-all-emp-leave]',
   templateUrl: './table-all-emp-leave.component.html',
   styleUrls: ['./table-all-emp-leave.component.css']
 })
