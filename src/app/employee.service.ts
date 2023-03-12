ac312476cd07d431c8e46a66e04f538cab5a018b
Cloud_1234 <cloudstoragemslm@gmail.com>
2023-03-12 13:57:34 +0300

    0

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -10,8 +10,12 @@ export class EmployeeService {
    }
 
    allLeaves = [
-    { "leaveid": 1, "startDate": "34234","endDate":"32432","Message":"tet","State":2,"documentFileName":"ss","message":"dfgdgtr","leaveType":"lev","fName":"Salah","LName":"Malouh"},
-    { "leaveid": 2, "startDate": "23432","endDate":"353","Message":"tet2","State":0,"documentFileName":"ss2","message":"asdfas","leaveType":"lev2","fName":"mohamde","LName":"ma2"},
-    { "leaveid": 3, "startDate": "2432","endDate":"234","Message":"tet3","State":1,"documentFileName":"ss3","message":"efewfewf","leaveType":"lev3","fName":"Moyeed","LName":"ma3"}
+    { "leaveid": 1, "startDate": "34234","endDate":"32432","State":2,"documentFileName":"ss","message":"dfgdgtr","leaveType":"lev","fName":"Salah","LName":"Malouh"},
+    { "leaveid": 2, "startDate": "23432","endDate":"353","State":0,"documentFileName":"ss2","message":"asdfas","leaveType":"lev2","fName":"mohamde","LName":"ma2"},
+    { "leaveid": 3, "startDate": "2432","endDate":"234","State":1,"documentFileName":"ss3","message":"efewfewf","leaveType":"lev3","fName":"Moyeed","LName":"ma3"}
   ];
+  allsultion = [
+    { "solutionid": 1, "taskid": "34234","uploadDate":"32432","feedMessage":"tet","State":2,"documentFileName":"srfwes"},
+    { "solutionid": 2, "taskid": "34234","uploadDate":"32432","feedMessage":"tet","State":0,"documentFileName":"ewfss"},   
+    { "solutionid":3, "taskid": "34234","uploadDate":"32432","feedMessage":"tet","State":1,"documentFileName":"wefss"},  ];
 }
--- src/app/employee/employee.module.ts
+++ src/app/employee/employee.module.ts
@@ -6,13 +6,17 @@ import { MainComponent } from './main/main.component';
 import { SharedModule } from '../shared/shared.module';
 import { GrtleavesComponent } from './grtleaves/grtleaves.component';
 import { LeavedetailComponent } from './leavedetail/leavedetail.component';
+import { GetsolutionComponent } from './getsolution/getsolution.component';
+import { SolutindetailComponent } from './solutindetail/solutindetail.component';
 
 
 @NgModule({
   declarations: [
     MainComponent,
     GrtleavesComponent,
-    LeavedetailComponent
+    LeavedetailComponent,
+    GetsolutionComponent,
+    SolutindetailComponent
   ],
   imports: [
     CommonModule,
