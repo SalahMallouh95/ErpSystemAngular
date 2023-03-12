6adec6624bba06ed76807e175136de49523e213b
SalahMallouh95 <SalahMalouh95@gmail.com>
2023-03-12 12:14:55 +0300

    1

--- src/app/home/contact/contact.component.ts
+++ src/app/home/contact/contact.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  constructor(private spinner: NgxSpinnerService) {}
+  ngOnInit(): void {
+    /** spinner starts on init */
+    this.spinner.show();
 
+    setTimeout(() => {
+      /** spinner ends after 5 seconds */
+      this.spinner.hide();
+    }, 3000);
+  }
 }
