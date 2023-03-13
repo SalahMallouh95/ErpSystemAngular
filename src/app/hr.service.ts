601d688fba893c64ed44422fc6f5560183b95822
SalahMallouh95 <SalahMalouh95@gmail.com>
2023-03-13 21:49:40 +0300

    2

--- src/app/hr.service.ts
+++ src/app/hr.service.ts
@@ -24,6 +24,7 @@ export class HrService {
  
 GetAllEmployee(){
   this.spinner.show();
+  
   this.http.get("https://localhost:44388/api/Hr/getuser").subscribe(
     {next:(res)=>{this.allEmp=res},
     error:(ee)=>{console.log(ee)}}
