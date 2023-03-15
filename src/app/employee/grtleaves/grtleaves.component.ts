6ec9e295965b04c7343d2e60acd4b593823075ef
Cloud_1234 <cloudstoragemslm@gmail.com>
2023-03-15 22:36:00 +0300

    0

--- src/app/employee/grtleaves/grtleaves.component.ts
+++ src/app/employee/grtleaves/grtleaves.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { EmployeeService } from 'src/app/employee.service';
+import {FormGroup, FormControl} from '@angular/forms';
+
 @Component({
   selector: 'app-grtleaves',
   templateUrl: './grtleaves.component.html',
@@ -11,7 +13,11 @@ export class GrtleavesComponent implements OnInit{
 constructor(public employeeService:EmployeeService,private router:Router){
 
 }
-  
+range = new FormGroup({
+  startdate: new FormControl<Date | null>(null),
+  enddate: new FormControl<Date | null>(null),
+});
+
 ngOnInit(): void {
     this.leaves.userid=1;
     this.employeeService.GetAllleave(this.leaves);
@@ -26,5 +32,8 @@ async GetValues(id :any){
   this.leave.leaveid=id
   await this.employeeService.GetleaveById(this.leave)
   this.router.navigate(['Employee/leavedetail']);
+}
+async Search(){
+  await this.employeeService.Search(this.range.value)
 }
 }
