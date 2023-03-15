e042373b79026c7637fa0e4ead79823e37a158cd
SalahMallouh95 <SalahMalouh95@gmail.com>
2023-03-15 22:16:21 +0300

    1

--- src/app/hr/all-emp-leaves/all-emp-leaves.component.ts
+++ src/app/hr/all-emp-leaves/all-emp-leaves.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HrService } from 'src/app/hr.service';
 import {FormGroup, FormControl} from '@angular/forms';
-import { DatePipe } from '@angular/common';
 
 
 
@@ -15,7 +14,7 @@ import { DatePipe } from '@angular/common';
 })
 export class AllEmpLeavesComponent implements OnInit {
   
-  constructor(private router:Router,public hrService:HrService,private datePipe: DatePipe)
+  constructor(private router:Router,public hrService:HrService)
   {
 
   }  
@@ -35,14 +34,8 @@ export class AllEmpLeavesComponent implements OnInit {
   
 
   async Search(){
-    const startDate = this.range.get('startdate')?.value;
-    const endDate = this.range.get('enddate')?.value;
     
-    const formattedStartDate = this.datePipe.transform(startDate, 'yyyy/MM/dd HH:mm:ss');
-    const formattedEndDate = this.datePipe.transform(endDate, 'yyyy/MM/dd HH:mm:ss');
-
-    // Use the formatted start and end dates in your search logic
-    console.log(formattedStartDate, formattedEndDate);
     await this.hrService.Search(this.range.value)
+    //if(this.range.)
   }
 }
