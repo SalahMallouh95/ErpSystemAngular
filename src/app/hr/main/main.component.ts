729e8e9dbdf03c38e4f72762b97780b1e94e7c97
SalahMallouh95 <SalahMalouh95@gmail.com>
2023-03-23 21:06:38 +0300

    3

--- src/app/hr/main/main.component.ts
+++ src/app/hr/main/main.component.ts
@@ -8,16 +8,33 @@ import { HrService } from 'src/app/hr.service';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent {
-    
-  constructor(public hrService:HrService,private auth:AuthService){}
+  
+  empCount:any
+  empOnCount:any
+  empOfCount:any
+  manCount:any
+  DepCount:any
+
+  constructor(public hrService:HrService,private auth:AuthService)
+  {
+
+  }
 
  async ngOnInit(){
     let userData:any = JSON.parse( localStorage.getItem('userInfo')+'')   
     userData.userid=parseInt (userData.userid)   
     userData.roleid=parseInt (userData.roleid)        
-    delete userData.exp          
-   
-      
-    localStorage.setItem('fullUserInfo',JSON.stringify(this.hrService.empInfo))            
+    delete userData.exp                  
+    localStorage.setItem('fullUserInfo',JSON.stringify(this.hrService.empInfo))    
+    await this.hrService.GetAllEmployee()
+    await this.hrService.GetAllDepartment()
+    await this.hrService.GetAllLeaves()
+    this.empCount=this.hrService.allEmp.length
+    this.empOnCount=this.hrService.allEmp.filter((e:any)=>e.state==1).length
+    this.empOfCount=this.hrService.allEmp.filter((e:any)=>e.state==0).length
+    this.manCount=this.hrService.allEmp.filter((e:any)=>e.roleid==2).length
+    this.DepCount=this.hrService.allDep.length
+
+            
   }
 }
